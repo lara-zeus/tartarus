bbe1aee8ecd37f6e5eea7a4ff0bad42ebe8822ba
Ash Monsh <wh7r.com@gmail.com>
2025-06-11 22:28:49 +0300

    updates

--- src/Filament/Schemata/Company/SettingsForm.php
+++ src/Filament/Schemata/Company/SettingsForm.php
@@ -14,6 +14,7 @@ class SettingsForm
     {
         return [
             Section::make()
+                ->columnSpanFull()
                 ->schema([
                     MultiLang::make('name')
                         ->live()
